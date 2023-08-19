d96e8cfca613dcf5c1d6dcdd0866d67ae7bef458
NasuTeodor <enderreal1@gmail.com>
2023-08-19 04:10:44 +0000

    get packets init decode

--- src/Monitoring/Interface.jsx
+++ src/Monitoring/Interface.jsx
@@ -2,18 +2,54 @@ import React, { useEffect, useState } from 'react'
 
 export default function Interface() {
 
+    // https://gist.github.com/unitycoder/a82365a93c9992f7f9631741fe007e9d
+    // LINK_TYPE
+    // PAYLOAD:
+    //     SHOST       (SOURCE ADDR)
+    //     DHOST       (DESTINATION ADDR)
+    //     ETHER TYPE  (TRANSFER PROTOCOL)
+    //     VLAN        (-)
+    //     PAYLOAD:
+                // TTL     (TIME TO LIVE)
+                // SADDR   (SOURCE IP)
+                // DADDR   (DESTINATION IP)
+                // PROTOCOL(COMM PROTOCOL NUMBER)
+                // LENGTH  (PAYLOAD LENGTH)
+                // VERSION ()
+
+    // PCAP HEADER:
+    //     CAP LENGTH  (PACKET SIZE) //one of this is the original size
+    //     LEN         (PACKET SIZE) //and the other one is the actual
+    //     TV SEC      (RECIEVE TIME)
+    //     TC USEC     (-)
+
     const socket = new WebSocket("ws://localhost:3005");
-    const [mess, setMess] = useState('')
+
+    const [packet, setPacket] = useState('')
 
     socket.addEventListener('open', () => { console.log('socket connected') })
-    // socket.addEventListener('data', (raw_data) => { setMess(raw_data) })
 
-    // socket.onmessage = function (raw_data) { setMess(raw_data) }
-    socket.on('message', function message(raw_data) {
-        setMess(raw_data)
-    })
+    socket.onmessage = function (raw_data) {
+        let packet = JSON.parse(raw_data.data)
+        console.log(packet)
+        setPacket(packet)
+    }
 
     return (
-        <div>{mess}</div>
+        <>
+            {packet.length !== 0 &&
+                < div >
+                    <div>Link Type: {packet.link_type}</div>
+                    <div>Payload:<br />
+                        {
+                            // console.log(Object.keys(packet.payload))
+                            Object.keys(packet.payload).forEach((key)=>{
+                                return(<div>{key}:{packet.payload[key]}<br/></div>)
+                            })
+                        }
+                    </div>
+                </div >
+            }
+        </>
     )
 }
